d8efe1e3ebe45b54582312f7f8d92e89127d41d2
bye9 <bye_9@naver.com>
2023-04-25 21:35:07 +0900

    [CHORE] pod RealmSwift 추가

---
+++ CalendarMap/CalendarMap/RealmManager.swift
@@ -0,0 +1,11 @@
+//
+//  RealmManager.swift
+//  CalendarMap
+//
+//  Created by JeongHwan Seok on 2023/04/20.
+//
+
+import Foundation
+import RealmSwift
+
+
--- CalendarMap/Pods/Target Support Files/Pods-CalendarMap/Pods-CalendarMap-frameworks.sh
+++ CalendarMap/Pods/Target Support Files/Pods-CalendarMap/Pods-CalendarMap-frameworks.sh
@@ -177,11 +177,15 @@ code_sign_if_enabled() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/FloatingPanel/FloatingPanel.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Realm/Realm.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RealmSwift/RealmSwift.framework"
   install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/NMapsGeometry/NMapsGeometry.framework"
   install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/NMapsMap/NMapsMap.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/FloatingPanel/FloatingPanel.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Realm/Realm.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RealmSwift/RealmSwift.framework"
   install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/NMapsGeometry/NMapsGeometry.framework"
   install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/NMapsMap/NMapsMap.framework"
 fi
