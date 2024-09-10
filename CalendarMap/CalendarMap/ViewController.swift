4a35fb4b3f7ea6a30a8f551bc2345235c79b115a
bye_9@naver.com <>
2024-09-10 21:46:57 +0900

    [FIX] 지도 심벌 탭 이벤트 제거

--- CalendarMap/CalendarMap/ViewController.swift
+++ CalendarMap/CalendarMap/ViewController.swift
@@ -253,19 +253,19 @@ extension ViewController: NMFMapViewTouchDelegate {
     ///   - mapView: 지도객체
     ///   - symbol: 탭된 심벌
     /// - Returns: YES일 경우 이벤트를 소비합니다. 그렇지 않을 경우 이벤트가 지도로 전달되어 mapView:didTapMap:point:가 호출됩니다.
-    func mapView(_ mapView: NMFMapView, didTap symbol: NMFSymbol) -> Bool {
-        dump(mapView)
-        dump(symbol)
-        //        <NMFSymbol:position=<NMGLatLng: 37.67111706221642,126.7377662658691>, caption=대화동 레포츠공원>
-        
-        let dataSource = NMFInfoWindowDefaultTextSource.data()
-        dataSource.title = symbol.caption
-        infoWindow.dataSource = dataSource
-        infoWindow.position = NMGLatLng(lat: symbol.position.lat, lng: symbol.position.lng)
-        infoWindow.open(with: mainMapView)
-        
-        return true
-    }
+//    func mapView(_ mapView: NMFMapView, didTap symbol: NMFSymbol) -> Bool {
+//        dump(mapView)
+//        dump(symbol)
+//        //        <NMFSymbol:position=<NMGLatLng: 37.67111706221642,126.7377662658691>, caption=대화동 레포츠공원>
+//        
+//        let dataSource = NMFInfoWindowDefaultTextSource.data()
+//        dataSource.title = symbol.caption
+//        infoWindow.dataSource = dataSource
+//        infoWindow.position = NMGLatLng(lat: symbol.position.lat, lng: symbol.position.lng)
+//        infoWindow.open(with: mainMapView)
+//        
+//        return true
+//    }
 }
 
 extension ViewController: UITextFieldDelegate {
